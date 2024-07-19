dc3a0066ca7c5f30d630ceacf9839f0838dd4979
Keren Duchan <keren.duchan@gmail.com>
2024-07-19 12:50:39 +0300

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test Trello Icons
+
+Testing the icons grabbed from trello.com, for implementing a Trello clone.
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <div className="app">
+            <h1>Trello Icons</h1>
+
+            <div className="icon icon-board">Board</div>
+            <div className="icon icon-label">Label</div>
+            <div className="icon icon-calendar">Calendar</div>
+        </div>
+    )
+}
+
+export default App
